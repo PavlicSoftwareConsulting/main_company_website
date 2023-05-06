acbdea2a915644781ad3bf95a38fe4ce944d74bf
Jordan Pavlic <jpavlic92@gmail.com>
2023-05-05 20:27:36 -0400

    Add header to webpage

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -2,8 +2,69 @@
 </script>
 
 <header>
+    <nav class="navbar">
+        <a href="#" class="navbar-brand">MyBrand</a>
+        <ul class="navbar-menu">
+            <li class="navbar-item">
+                <a href="#" class="navbar-link">Home</a>
+            </li>
+            <li class="navbar-item">
+                <a href="#" class="navbar-link">About</a>
+            </li>
+            <li class="navbar-item">
+                <a href="#" class="navbar-link">Services</a>
+            </li>
+            <li class="navbar-item">
+                <a href="#" class="navbar-link">Contact</a>
+            </li>
+        </ul>
+    </nav>
 </header>
 
 <style>
+    /* Reset default styles */
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    /* Style the navigation bar container */
+    .navbar {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        background-color: #333;
+        padding: 1rem;
+    }
+
+    /* Style the brand link */
+    .navbar-brand {
+        font-size: 1.5rem;
+        font-weight: bold;
+        color: white;
+        text-decoration: none;
+    }
+
+    /* Style the menu list and items */
+    .navbar-menu {
+        display: flex;
+        list-style-type: none;
+    }
+
+    .navbar-item {
+        margin-left: 1rem;
+    }
+
+    /* Style the menu links */
+    .navbar-link {
+        color: white;
+        text-decoration: none;
+        transition: color 0.3s;
+    }
+
+    .navbar-link:hover {
+        color: #f2f2f2;
+    }
 </style>
 
